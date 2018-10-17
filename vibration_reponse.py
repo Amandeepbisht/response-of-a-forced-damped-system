c17c90a254c8aad132bb48f641a2c8d19008c7cf
Amandeepbisht <37806285+Amandeepbisht@users.noreply.github.com>
2018-10-17 08:06:27 -0400

    Update vibration_reponse.py

--- vibration_reponse.py
+++ vibration_reponse.py
@@ -2,11 +2,11 @@ import numpy as np
 from scipy.integrate import odeint
 import matplotlib.pyplot as plt
 
-def function(y):
+def function(y): # function that defines w/wn (wn = natural frequency, w = varying input frequency)
     z = (1-y**2)**2 + (2*0.33*y)**2
     return (1/np.sqrt(z))
 
-w = np.linspace(0,140,500)
+w = np.linspace(0,140,500) # range of w(input natural frequency) to be used
 
 
 plt.plot(w,function(w))
