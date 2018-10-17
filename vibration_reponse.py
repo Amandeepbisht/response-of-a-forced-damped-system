a66e9e31bc58371b161d0c8325013855590088cb
Amandeepbisht <37806285+Amandeepbisht@users.noreply.github.com>
2018-10-17 14:22:18 -0400

    Update vibration_reponse.py

--- vibration_reponse.py
+++ vibration_reponse.py
@@ -2,11 +2,11 @@ import numpy as np
 from scipy.integrate import odeint
 import matplotlib.pyplot as plt
 
-def function(y): # function that defines w/wn (wn = natural frequency, w = varying input frequency)
+def function(y): # function that defines A/Xs in terms of w/wn (wn = natural frequency, w = varying input frequency)
     z = (1-y**2)**2 + (2*0.33*y)**2
     return (1/np.sqrt(z))
 
-w = np.linspace(0,140,500) # range of w(input natural frequency) to be used
+w = np.linspace(0,70,500) # here '70' is the 104/wn (wn = natural frequency of the system) and 500 is the number of divisions from 0 to 70
 
 
 plt.plot(w,function(w))
