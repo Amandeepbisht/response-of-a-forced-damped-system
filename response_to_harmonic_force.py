92fd13c40a9cddc0b57ef96950084578cccc0f25
Amandeepbisht <37806285+Amandeepbisht@users.noreply.github.com>
2018-10-17 07:55:46 -0400

    Update response_to_harmonic_force.py

--- response_to_harmonic_force.py
+++ response_to_harmonic_force.py
@@ -1,12 +1,9 @@
-
-
-
 import numpy as np
 from scipy.integrate import odeint
 import matplotlib.pyplot as plt
-from matplotlib import interactive
 
-def model(x,t):
+
+def model(x,t): # Function that defines the differential equation of the problem
     y = x[0]
     dy = x[1]
     xdot = [[],[]]
@@ -15,33 +12,19 @@ def model(x,t):
     return (xdot)
 
 
-t = np.linspace(0,15,1000)
+t = np.linspace(0,15,1000) 
 
-sol = odeint(model,[0.1,0.04],t)
+sol = odeint(model,[0.1,0.04],t) #solver function that solves the differential equation defined above
 print (sol[:,1])
 
 plt.plot(t,sol[:,1])
 plt.xlabel("time(seconds)")
 plt.ylabel("distance(meters)")
 plt.grid()
-interactive(True)
 plt.show()
 
 
 
-'''def function(y):
-    z = (1-y**2)**2 + (2*0.33*y)**2
-    return (1/np.sqrt(z))
-
-w = np.linspace(0,140,500)
-
-
-plt.plot(w,function(w))
-plt.xlabel("w/wn")
-plt.ylabel("A/Xs")
-plt.grid()
-interactive(False)
-plt.show()'''
 
 
 
